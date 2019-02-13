5b304ed4044905acbb28a90e1ea75b410b4e8e63
Ava <a.v.a.r.heinonen@gmail.com>
2019-02-13 16:38:53 +0200

    4.4 - 4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,48 @@
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
     return 1
   }
 
+const totalLikes = (blogs) => {
+  if(blogs.length === 0){
+    return 0
+  } else {
+    const likes = blogs.map(blog => blog.likes)
+    return likes.reduce(add, 0)
+  }
+  function add(a,b) {
+    return a + b
+  }
+}
+
+const favoriteBlog = (blogs) => {
+  if(blogs.length === 0){
+    return {}
+  } else {
+    let maxLikes = Math.max.apply(null,blogs.map(blog => blog.likes))
+    let favoriteBlog = blogs.filter(blog => blog.likes === maxLikes)
+    return favoriteBlog[0]
+  }
+}
+
+const mostBlogs = (blogs) => {
+  if(blogs.length === 0){
+    return {}
+  } else {
+    let blogsByAuthor = lodash.orderBy(lodash.groupBy(blogs, 'author'),'length')
+    const authorWithMostBlogs = lodash.takeRight(blogsByAuthor)
+    const result = {
+      name: authorWithMostBlogs[0][0].author,
+      blogs: authorWithMostBlogs[0].length
+    }
+  return result
+  }
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog,
+    mostBlogs
 }
